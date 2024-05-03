dc1286560b3a412b4a5e36a2ff06c7df1c40c00f
Lars Petter Bø <115019135+LarsPetter98@users.noreply.github.com>
2024-05-03 12:35:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_cors import CORS
+import secrets
+import os
+from datetime import timedelta
+from userModel import db
+from routes import main_bp
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+print(app.static_folder)  # Check the static folder path
+
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db.init_app(app)
+
+app.register_blueprint(main_bp)
+
+app.secret_key = secrets.token_hex(16)
+app.permanent_session_lifetime = timedelta(days=3)
 
 if __name__ == "__main__":
     app.run(debug=True)
